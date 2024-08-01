26c3788520b7f21ef267b3ae80e72fab793e6763
Ben018 <bensonwlee@gmail.com>
2024-08-01 16:03:42 -0400

    add basic header UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { AllRoutes } from "./routes/AllRoutes";
 import "./App.css";
+import { Header, Footer } from "./components";
 
 function App() {
   return (
     <div>
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
