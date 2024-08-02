c53cdd02d0f59d7ca16f30a642f7d2e619dd2115
Ben018 <bensonwlee@gmail.com>
2024-08-02 14:19:51 -0400

    add dark mode function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/react.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkMode")) || false
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
 
   const activeClass =
     "text-base block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
@@ -12,7 +25,7 @@ export const Header = () => {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={Logo} className="h-8" alt="MovieDB Logo" />
@@ -20,7 +33,41 @@ export const Header = () => {
               MovieDB
             </span>
           </Link>
-          <div className="flex md:order-2">
+
+          <div id="mobile-nav" className="flex md:order-2">
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              id="theme-toggle"
+              type="button"
+              className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+            >
+              {darkMode ? (
+                <svg
+                  id="theme-toggle-light-icon"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  id="theme-toggle-dark-icon"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              )}
+            </button>
+
             <button
               onClick={() => setHidden(!hidden)}
               type="button"
@@ -98,11 +145,12 @@ export const Header = () => {
               </svg>
             </button>
           </div>
+
           <div
+            id="nav-links"
             className={`${
               hidden ? "hidden" : ""
             } items-center justify-between w-full md:flex md:w-auto md:order-1`}
-            id="navbar-search"
           >
             <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -129,6 +177,7 @@ export const Header = () => {
                 placeholder="Search..."
               />
             </div>
+
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
